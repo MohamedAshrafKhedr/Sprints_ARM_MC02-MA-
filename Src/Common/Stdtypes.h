a4566df3a84ca9eaeef37cd3dad906edbdfc3fa0
MohamedAshrafKhedr <112961702+MohamedAshrafKhedr@users.noreply.github.com>
2022-09-17 16:30:19 +0200

    init commit

---
+++ Src/Common/Stdtypes.h
@@ -0,0 +1,21 @@
+/*
+ * Stdtypes.h
+ *
+ *  Created on: Dec 12, 2021
+ *      Author: 8
+ */
+
+#ifndef STDTYPES_H_
+#define STDTYPES_H_
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned long uint32;
+typedef signed char sint8;
+typedef signed short sint16;
+typedef signed long sint32;
+typedef float f32;
+typedef double f64;
+
+
+#endif /* STDTYPES_H_ */
